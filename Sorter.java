726562b3d642af030b6153dd8fbb0ae5658bc6a9
Khulud <khalodabied@gmail.com>
2023-09-15 10:10:52 +0300

    new method

--- Sorter.java
+++ Sorter.java
@@ -37,8 +37,21 @@ public class Sorter {
             arr[i] = smallerNumber;
         }
     }
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        int temp = 0;
+        for(int i=0; i < n; i++){
+            for(int j=1; j < (n-i); j++){
+                if(arr[j-1] > arr[j]){
+                    //swap elements
+                    temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
 
-    public static void main(String a[]){
+        public static void main(String a[]){
         int[] arr1 = {9,14,3,2,43,11,58,22};
         System.out.println("Before Insertion Sort");
         for(int i:arr1){
